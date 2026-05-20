5c74cb659872a308eacb05f1ceedb87d50ab8263
Mzgin L Ghazi <158284454+mzgindosky96@users.noreply.github.com>
2026-05-20 22:36:45 +0300

    Add files via upload
    Signed-off-by: Mzgin L Ghazi <158284454+mzgindosky96@users.noreply.github.com>

---
+++ config.php
@@ -0,0 +1,4 @@
+$db_host = 'localhost';     // Your database host
+$db_name = 'badini_movies'; // Database name  
+$db_user = 'root';          // Your MySQL username
+$db_pass = '';              // Your MySQL password
