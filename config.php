8c326aac0a46f93207f94790ace17148fcb533d2
Mzgin L Ghazi <158284454+mzgindosky96@users.noreply.github.com>
2026-05-21 04:55:17 +0300

    Delete config.php

--- config.php
+++
@@ -1,4 +0,0 @@
-$db_host = 'localhost';     // Your database host
-$db_name = 'badini_movies'; // Database name  
-$db_user = 'root';          // Your MySQL username
-$db_pass = '';              // Your MySQL password
